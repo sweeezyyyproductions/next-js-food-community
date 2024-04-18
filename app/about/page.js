1dd31c78c2147e5f870a8284da7e053fa03788af
Michael Simpson <100339112+sweeezyyyproductions@users.noreply.github.com>
2024-04-17 22:26:15 -0700

    Initial commit

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <main>
+            <h1>About Us</h1>
+            <p>Welcome to our website! We are a team of passionate individuals dedicated to providing high-quality products and services.</p>
+            <p>Feel free to explore our website and learn more about what we do.</p>
+        </main>
+    );
+}
